1a1833845680440ab24b63da97bdd4c603cc96d2
Sam Luby <sam.luby@ammeon.com>
2019-03-06 17:32:06 +0000

    marker on map

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import exifread as ef
 import re
+from gmplot import gmplot
+from datetime import datetime
 
 pic = 'images/phone_pic.jpg'
 
@@ -7,13 +9,32 @@ pic = 'images/phone_pic.jpg'
 def get_gps_coords(file):
     with open(pic, 'rb') as f:
         tags = ef.process_file(f)
+        # print(tags)
         lat = str(tags.get('GPS GPSLatitude'))
         lat_ref = str(tags.get('GPS GPSLatitudeRef'))
         lon = str(tags.get('GPS GPSLongitude'))
         lon_ref = str(tags.get('GPS GPSLongitudeRef'))
         lat = [lat, lat_ref]
         lon = [lon, lon_ref]
-        return lat, lon
+        f.close()
+    return lat, lon
+
+
+def get_date(file):
+    with open(pic, 'rb') as f:
+        tags = ef.process_file(f)
+        date = str(tags.get('Image DateTime'))
+        print(date)
+        f.close()
+    return date
+
+
+def format_date(date):
+    date, time = date.split(' ')
+    date = date.split(':')
+    date.reverse()
+    date = ('/').join(date)
+    print(date)
 
 
 def DMS_to_DD(dms_ref):
@@ -31,10 +52,23 @@ def DMS_to_DD(dms_ref):
     return dd
 
 
+def add_marker(map, lat, lon):
+    map.marker(lat, lon, 'cornflowerblue', title='test marker')
+
+
+
 def main():
     lat, lon = get_gps_coords(pic)
-    print(DMS_to_DD(lat))
-    print(DMS_to_DD(lon))
+    lat = DMS_to_DD(lat)
+    lon = DMS_to_DD(lon)
+
+    gmap = gmplot.GoogleMapPlotter(36.1, -115.2, 13)
+    add_marker(gmap, lat, lon)
+    gmap.draw("my_map.html")
+
+    date = get_date(pic)
+    format_date(date)
+
 
 
 if __name__ == '__main__':
